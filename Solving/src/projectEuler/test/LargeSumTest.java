f8eeab5774997b625b8c74e39d758e269a75c09f
Gek <gek.pandini@gmail.com>
2017-06-06 17:51:35 +0200

    Project Euler exercisse until 14

---
+++ Solving/src/projectEuler/test/LargeSumTest.java
@@ -0,0 +1,14 @@
+package projectEuler.test;
+
+import org.junit.Test;
+
+import projectEuler.implementation.LargeSum;
+
+public class LargeSumTest {
+
+	@Test
+	public void sum(){
+//		System.out.println(LargeSum.sum());
+		System.out.printf("dexp: %.0f\n", LargeSum.sum());
+	}
+}
