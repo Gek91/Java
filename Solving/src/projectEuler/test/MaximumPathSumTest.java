dbc356b098b9d5cb7a60887159ecc2136027f52f
Gek <gek.pandini@gmail.com>
2017-06-07 19:53:14 +0200

    Project euler until exercise 18

---
+++ Solving/src/projectEuler/test/MaximumPathSumTest.java
@@ -0,0 +1,15 @@
+package projectEuler.test;
+
+import org.junit.Test;
+
+import projectEuler.implementation.MaximumPathSum;
+
+public class MaximumPathSumTest {
+
+	@Test
+	public void calculateMaxSum(){
+		System.out.println(MaximumPathSum.calculateMaxSum());
+	}
+	
+}
+ 
